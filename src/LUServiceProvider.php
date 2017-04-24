6853171f13d18b5a0471c2c9d8bc3682f750e21b
bubu2k <marco76tv@hotmail.com>
2017-04-24 15:08:57 +0200

    first commit

---
+++ src/LUServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace XRA\LU;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+use XRA\Extend\Traits\ServiceProviderTrait;
+
+class LUServiceProvider extends ServiceProvider{
+     use ServiceProviderTrait;
+    
+}
