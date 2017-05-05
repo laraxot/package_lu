96bd18047e0f3b8b96ffe5cd344dfa4f3d1a7533
Raffaele <raffaele.carelle@outlook.com>
2017-05-05 15:04:40 +0200

    a

---
+++ src/migrations/2017_04_01_160406_create_liveuser_grouprights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLiveuserGrouprightsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (!Schema::connection('liveuser_general')->hasTable('liveuser_grouprights')) 
+		Schema::connection('liveuser_general')->create('liveuser_grouprights', function(Blueprint $table)
+		{
+			$table->integer('group_id')->unsigned()->default(0);
+			$table->integer('right_id')->unsigned()->default(0);
+			$table->boolean('right_level')->nullable()->default(3);
+			$table->primary(['group_id','right_id']);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection('liveuser_general')->drop('liveuser_grouprights');
+	}
+
+}
---
+++ src/migrations/2017_04_01_160406_create_liveuser_rights_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLiveuserRightsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		if (!Schema::connection('liveuser_general')->hasTable('liveuser_rights')) 
+		Schema::connection('liveuser_general')->create('liveuser_rights', function(Blueprint $table)
+		{
+			$table->integer('right_id')->unsigned()->default(0)->primary();
+			$table->integer('area_id')->unsigned()->default(0)->index('rights_area_id');
+			$table->string('right_define_name', 250);
+			$table->char('has_implied', 1)->default('N');
+			$table->char('has_level', 1)->default('N');
+			$table->unique(['area_id','right_define_name'], 'right_define_name');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::connection('liveuser_general')->drop('liveuser_rights');
+	}
+
+}
