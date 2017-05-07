f73a6d7611700f49d9e63cc97ee52f9ee8f3cd1f
Raffaele <raffaele.carelle@gmail.com>
2017-05-07 21:47:24 +0200

    A

--- src/views/admin/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('adm_theme::layouts.app')
-@section('page_heading','package lu')
-@section('section')
-@include('includes.flash')
-@include('includes.components')
-
-lu package ..
-
-@endsection
