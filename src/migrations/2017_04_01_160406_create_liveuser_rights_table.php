37d9a9ecc7bab398dc3e37369009504ae924b5d1
bubu2k <marco76tv@hotmail.com>
2017-05-09 09:13:11 +0200

    up

--- src/migrations/2017_04_01_160406_create_liveuser_rights_table.php
+++ src/migrations/2017_04_01_160406_create_liveuser_rights_table.php
@@ -15,15 +15,9 @@ class CreateLiveuserRightsTable extends Migration {
 		if (!Schema::connection('liveuser_general')->hasTable('liveuser_rights')) 
 		Schema::connection('liveuser_general')->create('liveuser_rights', function(Blueprint $table)
 		{
-<<<<<<< HEAD
-			$table->integer('right_id')->unsigned()->default(0)->primary();
-			$table->integer('area_id')->unsigned()->default(0)->index('rights_area_id');
-			$table->string('right_define_name', 250);
-=======
 			$table->increments('right_id');
 			$table->integer('area_id')->unsigned()->default(0)->index('rights_area_id');
 			$table->string('right_define_name', 150);
->>>>>>> master
 			$table->char('has_implied', 1)->default('N');
 			$table->char('has_level', 1)->default('N');
 			$table->unique(['area_id','right_define_name'], 'right_define_name');
--- src/migrations/2017_04_01_160406_create_liveuser_translations_table.php
+++ src/migrations/2017_04_01_160406_create_liveuser_translations_table.php
@@ -15,20 +15,13 @@ class CreateLiveuserTranslationsTable extends Migration {
 		if (!Schema::connection('liveuser_general')->hasTable('liveuser_translations')) 
 		Schema::connection('liveuser_general')->create('liveuser_translations', function(Blueprint $table)
 		{
-<<<<<<< HEAD
-=======
 			$table->increments('id');
->>>>>>> master
 			$table->integer('section_id')->unsigned()->default(0);
 			$table->boolean('section_type')->default(0);
 			$table->string('language_id', 2)->default('');
 			$table->string('name', 50)->default('');
 			$table->string('description')->nullable();
-<<<<<<< HEAD
-			$table->primary(['section_id','section_type','language_id']);
-=======
 			//$table->primary(['section_id','section_type','language_id']);//meglio usare id piu' veloce da far mangiare al crud
->>>>>>> master
 		});
 	}
 
