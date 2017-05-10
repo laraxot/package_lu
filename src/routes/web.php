4203d6bbee7a93f04b03873a4a6d1956f142b247
bubu2k <marco76tv@hotmail.com>
2017-05-10 11:32:33 +0200

    upp

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,3 +1,10 @@
 <?php
+//--- logout ovunque ma ha senso solo se sei dentro --
+$middleware= ['web','auth'];
+$namespace='XRA\LU\Controllers\Auth';
+Route::group(['prefix' => null, 'middleware' => $middleware, 'namespace' => $namespace]
+, function () {
+    Route::post('logout', ['as'=>'logout','uses'=>'LoginController@logout'] ); // non serve 'guest'
+});
 
 $this->routes();
