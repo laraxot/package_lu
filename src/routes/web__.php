53b5ea30c76699be2934423a64e1196ec6d7a972
Raffaele <raffaele.carelle@gmail.com>
2017-05-24 21:22:43 +0200

    a

--- src/routes/web__.php
+++ src/routes/web__.php
@@ -1,5 +1,5 @@
 <?php
-//die('['.__LINE__.']['.__FILE__.']');
+die('['.__LINE__.']['.__FILE__.']');
 $middleware=['web','guest'];
 $namespace='XRA\LU\Controllers\Auth';
 
@@ -12,15 +12,14 @@ Route::group(['prefix' => null, 'middleware' => $middleware, 'namespace' => $nam
     Route::post('password/email', ['as'=>'password.email','uses'=>'ForgotPasswordController@sendResetLinkEmail'] ); // non serve 'guest'
     Route::match(array('GET', 'HEAD'),'password/reset',[
     			'as'=>'password.request'
-    			,'uses'=>'ForgotPasswordController@showLinkRequestForm'] ); 
+    			,'uses'=>'ForgotPasswordController@showLinkRequestForm'] );
     Route::post('password/reset','ResetPasswordController@reset');
     Route::match(array('GET', 'HEAD'),'password/reset/{token}',[
     			'as'=>'password.reset'
-    			,'uses'=>'ResetPasswordController@showResetForm'] ); 
+    			,'uses'=>'ResetPasswordController@showResetForm'] );
 
     Route::match(array('GET', 'HEAD'),'register',[
     			'as'=>'register'
-    			,'uses'=>'RegisterController@showRegistrationForm'] ); 
+    			,'uses'=>'RegisterController@showRegistrationForm'] );
     Route::post('register','RegisterController@register');
-
 });
