8657203fa925ac8420623f485247abfa5cb3e825
pier <pierluigi.tonegato@gmail.com>
2017-06-22 23:51:48 +0200

    sistemate migrazioni

--- src/migrations/2014_10_12_100000_create_password_resets_table.php
+++ src/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,11 +14,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD
-        if (!Schema::connection('liveuser_general')->hasTable($this->table))
-=======
         if (!Schema::connection('liveuser_general')->hasTable($this->table)) 
->>>>>>> f275b5801e7fa85d2df2d6b07d2c5d92b0ee86e2
         Schema::connection('liveuser_general')->create($this->table, function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
