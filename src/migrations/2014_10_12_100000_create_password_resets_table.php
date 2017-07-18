7a583fc969ade2fd97c3cba7c93f5092302c6c88
bubu2k <marco76tv@hotmail.com>
2017-07-18 12:54:59 +0200

    messo limite alla stringa perche' se no da errore di indice troppo grande

--- src/migrations/2014_10_12_100000_create_password_resets_table.php
+++ src/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -16,9 +16,16 @@ class CreatePasswordResetsTable extends Migration
     {
         if (!Schema::connection('liveuser_general')->hasTable($this->table)) 
         Schema::connection('liveuser_general')->create($this->table, function (Blueprint $table) {
-            $table->string('email')->index();
+            //$table->increments('id');
+            //with index  SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+            //=> so i add ID increments, remember to test it
+            //$table->string('email'); 
+            //$table->string('email')->index();
+            $table->string('email',191)->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
+            //$table->rememberToken();
+            //$table->timestamps();
         });
     }
 
